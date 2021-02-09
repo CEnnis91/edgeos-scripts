53d87b4e11b32f01d398c61d213962101f47690f
CEnnis91 <cennis91@gmail.com>
2021-02-08 21:49:43 -0500

    Add tool to package the acme directory

---
+++ bin/reload/package_acme_certs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# package_acme_certs.sh - tars let's encrypt certificates
+
+SELF_DIR="$(dirname "$(readlink -f "$0")")"
+ROOT_DIR="$(dirname "$(dirname "$SELF_DIR")")"
+
+# shellcheck disable=SC1090
+. "${ROOT_DIR}/lib/acme.sh"
+
+TAR_FILE="${SSL_DIR}/acme.tar.gz"
+if [[ -f "$TAR_FILE" ]]; then
+	mv -f "$TAR_FILE" "${TAR_FILE}.old"
+fi
+
+if [[ -d "$ACME_DIR" ]]; then
+	( cd "$ETC_DIR"; tar czf "$TAR_FILE" "$ACME_DIR" )
+fi
