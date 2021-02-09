145359a125853058d9f74b3a459bdf1efd4aae39
CEnnis91 <cennis91@gmail.com>
2021-02-08 22:13:15 -0500

    Adjust paths

--- bin/reload/package_acme_certs.sh
+++ bin/reload/package_acme_certs.sh
@@ -7,11 +7,11 @@ ROOT_DIR="$(dirname "$(dirname "$SELF_DIR")")"
 # shellcheck disable=SC1090
 . "${ROOT_DIR}/lib/acme.sh"
 
-TAR_FILE="${SSL_DIR}/acme.tar.gz"
+TAR_FILE="${ETC_DIR}/acme.tar.gz"
 if [[ -f "$TAR_FILE" ]]; then
 	mv -f "$TAR_FILE" "${TAR_FILE}.old"
 fi
 
 if [[ -d "$ACME_DIR" ]]; then
-	( cd "$ETC_DIR"; tar czf "$TAR_FILE" "$ACME_DIR" )
+	( cd "$ETC_DIR"; tar czf "$TAR_FILE" "acme" )
 fi
